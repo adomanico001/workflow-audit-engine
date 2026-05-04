a3030adabd3ec049571f1ce29e1999938afb4d06
Adelina Domanico <102832335+adomanico001@users.noreply.github.com>
2026-05-04 14:27:19 -0400

    Update sampleData.js

--- sampleData.js
+++ sampleData.js
@@ -1,3 +1,21 @@
 const sampleRecords = [
-    {}, {}, {},
+    {
+        case_id: "C-1010",
+        client_id: "CL-1000",
+        client_tier: "Enterprise",
+        case_type: ,
+        priority: ,
+        status: ,
+        created_date: ,
+        last_updated_date: ,
+        resolution_date: ,
+        assigned_team: ,
+        has_missing_data: ,
+    },
+    {
+        
+    },
+    {
+        
+    },
 ]
